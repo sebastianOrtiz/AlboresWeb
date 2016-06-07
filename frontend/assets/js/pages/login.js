0051e0173823e6c073d1cd259ef7682b7fe1b2f8
Sebastian Ortiz Valencia <sebastianortiz989@gmail.com>
2016-06-07 18:22:41 -0500

    cambio source logo

--- frontend/assets/js/pages/login.js
+++ frontend/assets/js/pages/login.js
@@ -1,6 +1,5 @@
 $('#btnLogeo').click(function() {
 	if($('#emailLogeo').val() != "" && $('#passLogeo').val()!= ""){
-		alert("redireccionando");
 		window.location.href = '../../backend/pages/user.html';
 	}
 	
